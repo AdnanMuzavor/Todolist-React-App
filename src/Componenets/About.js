3140ef927ea2c215549a0d322a0a1acc8b58920b
Adnan Muzavor <www.adnan9168389872@gmail.com>
2021-05-01 11:48:25 +0530

    Adding some style to about

--- src/Componenets/About.js
+++ src/Componenets/About.js
@@ -1,9 +1,24 @@
 import React from 'react';
+
+
  export const About = () => {
+    let h1_styles={
+        justifycontent: "center",
+        alignitems: "center",
+        textalign:" center",
+        color: "brown",
+        backgroundcolor:" yellow",
+        padding:"20px"
+
+     }
+     let div_style={
+        justifycontent: "center",  
+    
+     }
      return (
-         <div>
-<h2>This about componenet!</h2>
-<p>
+         <div style={div_style} >
+<h2 style={h1_styles}>This about componenet!</h2>
+<p style={h1_styles}>
     This react app composed of several components group together to
   form this application so that <em>it's usedful for entering todos/todo items you wish to add.</em> It's also cabable of retaining your todo list incase you didn't deleted and bymistakenly reloaded the page.
   <strong>Thank you for reading!</strong>
--- src/Componenets/header.js
+++ src/Componenets/header.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import {Navbar, NavDropdown, Nav, Form, FormControl, Button} from 'react-bootstrap'
 export default function Header(){
     return(
 <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand to="#home">React-Bootstrap</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
+      <Nav.Link to="#home">Home</Nav.Link>
+      <Nav.Link to="#link">Link</Nav.Link>
       <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Item to="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item to="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item to="#action/3.3">Something</NavDropdown.Item>
         <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        <NavDropdown.Item to="#action/3.4">Separated link</NavDropdown.Item>
       </NavDropdown>
     </Nav>
     <Form inline>
